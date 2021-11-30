14f2d1df10a4763065f21c2828967a9a6898c122
Deo014 <ondra014@seznam.cz>
2021-11-30 20:36:24 +0100

    test 3

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <p>Hello World</p>
- </body>
+    <head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+        <link rel= "stylesheet" href= "css/style.css">
+        <title>PHP Test</title>
+    </head>
+
+    <body>
+        <p>Hello World</p>
+    </body>
 </html>
