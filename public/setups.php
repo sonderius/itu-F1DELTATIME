89bb2a477fa0b0dc27974f896c7b32316586f966
Deo014 <ondra014@seznam.cz>
2021-11-30 22:30:12 +0100

    setup

--- public/setups.php
+++ public/setups.php
@@ -11,7 +11,17 @@
             <a href="/calculator.php" class="tab-button">Calculator</a>
             <a href="/setups.php" class="tab-button" id="active-tab">My Setups</a>
             <a href="/tracks.php" class="tab-button">Tracks</a>
-        
+            
+            <h1>Season</h1>
+
+            <div class="radio-button">
+                <div class="season-button">
+                    <input type="radio" id="2019" name="season" />
+                    <label for="2019">2019</label>
+                    <input type="radio" id="2020" name="season" />
+                    <label for="2020">2020</label>
+                </div> <!-- season-button -->
+            </div> <!-- radio-button -->
         </div> <!-- horizontal-center -->
     </body>
 </html>
