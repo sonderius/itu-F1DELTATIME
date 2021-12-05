77c57553b53d16765877b026c5ead2c24604f678
sonderius <lubomir.zavodsky@outlook.sk>
2021-12-05 18:27:18 +0100

    return change

--- scripts/php/auth.php
+++ scripts/php/auth.php
@@ -26,12 +26,12 @@ if(isset($_POST['login']))
     $return_code = login($_POST['username'], $_POST['password']);
     if($return_code == 1)
     {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Location: /?badPassword');
         exit();
     }
     elseif($return_code == 2)
     {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        header('Location: /?unknownUser');
         exit();
     }
 }
